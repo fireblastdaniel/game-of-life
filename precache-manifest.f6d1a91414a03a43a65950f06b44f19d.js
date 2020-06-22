875e3219082baef31bb03c324b39b24508d55e02
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-06-22 13:03:06 -0700

    Updates

---
+++ precache-manifest.f6d1a91414a03a43a65950f06b44f19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cb7d75102991d99db7fdfd6b9570a5",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "b4fe7affd01202f238d0",
+    "url": "/game-of-life/static/css/main.33c2271f.chunk.css"
+  },
+  {
+    "revision": "7f1f865cf971767caf8d",
+    "url": "/game-of-life/static/js/2.2819d9cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/game-of-life/static/js/2.2819d9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fe7affd01202f238d0",
+    "url": "/game-of-life/static/js/main.ce3edeca.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
