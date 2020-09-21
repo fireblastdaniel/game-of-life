c58126e968d89dcefbcb3dafca259d9e04e6baee
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-09-21 16:30:20 -0600

    removed ga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import ReactGA from 'react-ga';
 
 import Home from './pages/home';
 import About from './pages/about';
@@ -9,17 +7,7 @@ import Game from './pages/game';
 
 import './App.css';
 
-ReactGA.initialize('UA-178647961-1');
-const browserHistory = createBrowserHistory();
-browserHistory.listen((location, action) => {
-  ReactGA.pageview(location.pathname + location.search);
-});
-
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-  return (
     <>
       <Route exact path='/'>
         <Home />
